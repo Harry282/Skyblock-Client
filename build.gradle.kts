16b62ce7d6d0b6a0297fd848b96abc1b20d2a56b
Harry282 <iamharry282@gmail.com>
2021-11-21 18:00:35 -0500

    Update dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import net.minecraftforge.gradle.user.TaskSingleReobf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.6.0"
     id("net.minecraftforge.gradle.forge") version "6f53277"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("org.spongepowered.mixin") version "0.6-SNAPSHOT"
@@ -46,7 +46,7 @@ dependencies {
     annotationProcessor("org.spongepowered:mixin:0.7.11-SNAPSHOT")
     implementation("org.spongepowered:mixin:0.7.11-SNAPSHOT")
     packageLib("gg.essential:loader-launchwrapper:1.1.2")
-    implementation("gg.essential:essential-1.8.9-forge:1541")
+    implementation("gg.essential:essential-1.8.9-forge:1566")
 }
 
 mixin {
