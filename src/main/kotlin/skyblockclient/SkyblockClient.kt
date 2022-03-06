6518d2d85d7c1002ec42560830c0283803983c6e
Harry282 <iamharry282@gmail.com>
2022-03-06 15:22:52 -0500

    Bump version and essential

--- src/main/kotlin/skyblockclient/SkyblockClient.kt
+++ src/main/kotlin/skyblockclient/SkyblockClient.kt
@@ -148,7 +148,7 @@ class SkyblockClient {
     companion object {
         const val MOD_ID = "sbclient"
         const val MOD_NAME = "Skyblock Client"
-        const val MOD_VERSION = "0.1.3-pre2"
+        const val MOD_VERSION = "0.1.3-pre3"
         const val CHAT_PREFIX = "§b§l<§fSkyblockClient§b§l>§r"
         val mc: Minecraft = Minecraft.getMinecraft()
         var config = Config
