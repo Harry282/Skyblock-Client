6d3c2428d811da57a68b86e1d47a68ebb1970799
Harry282 <iamharry282@gmail.com>
2022-03-16 15:49:51 -0400

    Update norotate

--- src/main/kotlin/skyblockclient/features/NoRotateHook.kt
+++ src/main/kotlin/skyblockclient/features/NoRotateHook.kt
@@ -11,12 +11,13 @@ object NoRotateHook {
     private var prevYaw = 0f
 
     fun handlePlayerPosLookPre() {
-        prevPitch = mc.thePlayer.rotationPitch
-        prevYaw = mc.thePlayer.rotationYaw
+        if (mc.thePlayer == null || mc.thePlayer.rotationPitch % 1 == 0f && mc.thePlayer.rotationYaw % 1 == 0f) return
+        prevPitch = mc.thePlayer.rotationPitch % 360
+        prevYaw = mc.thePlayer.rotationYaw % 360
     }
 
     fun handlePlayerPosLook(packet: S08PacketPlayerPosLook) {
-        if (!config.noRotate || !inSkyblock || packet.pitch % 1 == 0f) return
+        if (!config.noRotate || !inSkyblock || packet.pitch % 1 == 0f && packet.yaw % 1 == 0f) return
         mc.thePlayer.run {
             rotationPitch = prevPitch.also { prevRotationPitch = it }
             rotationYaw = prevYaw.also { prevRotationYaw = it }
